cc564114de1e44f9f0f2066383de6667e406d9f9
Eileen <eileen.tallman@gmail.com>
2019-03-12 14:10:42 -0400

    Added component folders

---
+++ src/components/login/loginSubmitButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LoginButton extends Component{
+    render() {
+        return (
+            <div className="Login-Button">
+                <button>Login</button>
+            </div>
+    
+        )}
+
+}
+
+export default LoginButton
