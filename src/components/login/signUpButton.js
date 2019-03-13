21ae4334581477500d4a113a43da475e067275bc
Eileen <eileen.tallman@gmail.com>
2019-03-13 10:56:54 -0400

    Made sure classnames, labels, names, and component/exports are consistent and standard across component pages

--- src/components/login/signUpButton.js
+++ src/components/login/signUpButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class SignUpButton extends Component{
     render() {
         return (
-            <div className="sign-Up-Button">
+            <div className="sign-up-button">
                 <button onClick={this.props.handleClick}>Sign Up</button>
             </div>
     
--- src/components/profile/userBio.js
+++ src/components/profile/userBio.js
@@ -5,7 +5,7 @@ class UserBio extends Component{
     render() {
         return (
             
-            <div className="profileUserBioDiv">
+            <div className="profile-user-bio-div">
               <div className="profile-bio">
                    <label for="profileBio">About Me:</label>
                     <input 
