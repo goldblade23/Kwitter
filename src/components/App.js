0b3c3c5f0dbc943e78d7242676df885600368ebb
KashF <Kashfarhadi@gmail.com>
2019-03-15 14:36:19 -0400

    Fixed capital class names

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React, { Component } from "react";
+import "../App.css";
 
-import Title from "./title.js"
+import Title from "./title.js";
 
-import LoginPage from "./login/loginPage.js"
-import ProfilePage from "./profile/profilePage.js"
+import LoginPage from "./login/loginPage.js";
+import ProfilePage from "./profile/profilePage.js";
 
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 class App extends Component {
-//   state= {
-//     active:false
-//   }
+  //   state= {
+  //     active:false
+  //   }
 
-//   handleClick = event =>{
-//    this.setState({active: !this.state.active})
-   
-//  }
+  //   handleClick = event =>{
+  //    this.setState({active: !this.state.active})
 
-  
+  //  }
+
   render() {
     return (
       <div className="App">
+        <Title />
+        <Switch>
+          <Route exact path="/" render={() => <p>hello</p>} />
 
-      <Title />
-      <Switch>
-       
-    <Route exact path="/" render={() =>
-      <p>hello</p>
-      } />
+          <Route exact path="/login" render={() => <LoginPage />} />
 
-  <Route exact path="/login" render={() =>
-     <LoginPage />
-      } />    
+          <Route exact path="/profile" render={() => <ProfilePage />} />
 
-<Route exact path="/profile" render={() =>
- <ProfilePage />
-} /> 
-
-<Route exact path="/feed" render={() =>
-          <p>feed</p>
-        } /> 
-          </Switch>
+          <Route exact path="/feed" render={() => <p>feed</p>} />
+        </Switch>
       </div>
     );
   }
--- src/components/login/loginPage.js
+++ src/components/login/loginPage.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import SignUpForm from "./signUp.js"
+import React, { Component } from "react";
+import SignUpForm from "./signUp.js";
 import { connect } from "react-redux";
-import { loginThenGoToUserProfile as login } from "../../actions/auth"
+import { loginThenGoToUserProfile as login } from "../../actions/auth";
 import Spinner from "react-spinkit";
 
-class LoginPage extends Component{
-    state = { username: "", password: "" };
+class LoginPage extends Component {
+  state = { username: "", password: "" };
 
   handleLogin = e => {
     e.preventDefault();
@@ -15,65 +15,60 @@ class LoginPage extends Component{
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
-    
-    
-    
-    
-    render() {
-        const { isLoading, err } = this.props;
-        return (
-         <div className="login-page" >
-          <div className="login-form">
-            <form onSubmit={this.handleLogin}>
-                <div className="login-section">
-                    <h2>LOGIN:</h2>  
-                    <div className="login-user-name">
-                    <label htmlFor="username">USERNAME:</label>
-                        <input 
-                            type="text"  
-                            name="username"
-                            autoFocus
-                            required
-                            onChange={this.handleChange}
-                            />
-                            
-                    </div>
 
-                    <div className="login-password">
-                    <label htmlFor="password">PASSWORD:</label>
-                        <input 
-                        type="password"
-                        name="password"
-                        required
-                        onChange={this.handleChange}/>
-                    </div>
-                    <div className="login-button">
-                <button type="submit" disabled={isLoading}>Login</button>
+  render() {
+    const { isLoading, err } = this.props;
+    return (
+      <div className="login-page">
+        <div className="login-form">
+          <form onSubmit={this.handleLogin}>
+            <div className="login-section">
+              <h2>LOGIN:</h2>
+              <div className="login-user-name">
+                <label htmlFor="username">USERNAME:</label>
+                <input
+                  type="text"
+                  name="username"
+                  autoFocus
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div className="login-password">
+                <label htmlFor="password">PASSWORD:</label>
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="login-button">
+                <button type="submit" disabled={isLoading}>
+                  Login
+                </button>
+              </div>
             </div>
-                </div>
-            </form>
-            {isLoading && <Spinner name="circle" color="blue" />}
-            {err && <p style={{ color: "red" }}>{err}</p>}
+          </form>
+          {isLoading && <Spinner name="circle" color="blue" />}
+          {err && <p style={{ color: "red" }}>{err}</p>}
 
-
-            {/* <div className="sign-Up-Button">
+          {/* <div className="sign-Up-Button">
                 <button onClick={()=>this.props.signUpOpenForm()}>Sign Up</button>
             </div> */}
-            
- <SignUpForm />
-  
-  
-            </div>
-</div>
-        )} 
+
+          <SignUpForm />
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
 export default connect(
-    ({ auth }) => ({
-      isLoading: auth.loginLoading,
-      err: auth.loginError
-    }),
-    { login }
-  )(LoginPage)
+  ({ auth }) => ({
+    isLoading: auth.loginLoading,
+    err: auth.loginError
+  }),
+  { login }
+)(LoginPage);
