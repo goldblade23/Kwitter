e7e2d40c10f153e1fef2b73c28c158265639fd96
Eileen <eileen.tallman@gmail.com>
2019-03-18 10:54:41 -0400

    Updated profile page with handlers and nav links

---
+++ src/actions/users.js
@@ -0,0 +1,7 @@
+//create action creator function to receive updated info
+//(password, username) which comes from the component layer
+
+//fetch request inside action creator function
+//connect, mapdispatch to props will wrap the action creator call in dispatch
+//action creator object goes to the reducer
+
