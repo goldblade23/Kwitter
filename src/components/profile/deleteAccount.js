c2c985d66f6443412eb4c8dd2808f33774d0878c
Eileen <eileen.tallman@gmail.com>
2019-03-19 11:12:34 -0400

    updated profile changes

--- src/components/profile/deleteAccount.js
+++ src/components/profile/deleteAccount.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
-import { deleteAccount } from "./actions.js"
+// import { deleteAccount } from "./actions.js"
 class DeleteAccount extends Component {
   render() {
-    const {isLoading} = this.props;
+    // const {isLoading} = this.props;
     return (
       <div className="delete-account-div">
         <h2>Warning: Deleting your account is permanent.</h2>
         <h3>You will lose complete access to your account.</h3>
         <button onClick={this.props.deleteAccount}> I understand. Please delete my account.</button>
-         <button type="submit" disabled={isLoading} to="/profile">I want to keep my account! Take me back to my account settings.</button>
+         <button type="submit"  to="/profile">I want to keep my account! Take me back to my account settings.</button>
+      {/* disabled={isLoading} */}
       </div>
     );
   }
@@ -21,7 +22,7 @@ function mapStateToProps() {
   }
 }
 const mapDispatchToProps = {
-  deleteAccount
+ 
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DeleteAccount);
--- src/components/profile/profilePage.js
+++ src/components/profile/profilePage.js
@@ -1,95 +1,122 @@
 import React, { Component } from "react";
 import UserPic from "./userPic.js";
-import DeleteAccount from "./deleteAccount";
-import { NavLink} from "react-router-dom";
-import { user } from "../../actions"
+// import { user } from "../../actions"
 import { connect } from "react-redux";
 
-class ProfilePage extends Component {
-  state = { displayName: "", about: "" }
+// import DeleteAccount from "./deleteAccount";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+  class ProfilePage extends Component {
 
+    state = {
+      active: false,
+      changeCurrentPassword: false,
+      passwordValue: "",
+      displayName: "",
+      about: ""
+
+    }
+
+    addNewPassword = (e) => {
+      if (e.key === "Enter" && this.state.value !== "") {
+        console.log(this.state.value)
+        this.props.addNewPassword(this.state.value)
+        e.target.value = ""
+      }
+    }
+    handleInputChange = (e) => {
+      this.setState({ value: e.target.value });
+    };
   render() {
-    const {isLoading} = this.props;
+    // const {isLoading} = this.props;
     return (
-      <div className="profile-page">
-        <UserPic />
-        <form>
-          
-          <div className="profile-user-info-div">
-            <div className="profile-username">
-              <label htmlFor="username">USERNAME:{this.props.username}</label>
-              </div>
+
+          <Router>
+            <div className="profile-page">
+              <UserPic />
+              <form>
+                <div className="profile-display-Name">
+                  <label htmlFor="displayName">DISPLAYNAME:</label>
+                  <input type="text" name="displayName" placeholder="(login.displayname)" />
+                </div>
+    
+                <div className="profile-user-info-div">
+                  <div className="profile-username">
+                    <label htmlFor="username">USERNAME:</label>
+                    <input
+                      type="text" name="username"
+                      placeholder />
+                  </div>
+                  <div className="current-password-div">
+                    <label for="currentPasswordSetting">PASSWORD:</label>
+                    <input
+                      type="text"
+                      placeholder="......"
+                      name="username" />
+                    <NavLink exact to='/' activeClassName='selected'>Change Current Password</NavLink>
+                  </div>
+                </div>
+                <div className="profile-user-bio-div">
+                  <div className="profile-bio">
+                    <label>
+                      About me:
+                     <textarea rows="4" cols="40" name="about" />
+                    </label>
+                  </div>
+                </div>
+                <div className="delete-account-button">
+                  <button to="/warning">Delete Account</button> 
+                </div>
+                <div className="profile-return-to-feed">
+                  <button type="submit" disabled={isLoading} to="/feed">
+                    Back to My Feed
+            </button>
+    
+                </div>
+                <div className="profile-Submit-Button">
+                  <button>Submit Changes</button>
+                </div>
+              </form>
             </div>
-
-            <div className="profile-display-Name">
-              <label htmlFor="displayName">DISPLAYNAME:</label>
-              <input type="text" name="displayName"/>
+          </Router>
+        );
+      }
+    }
+    
+    class UserPic extends Component {
+      state = {
+        active: false,
+        selectedFile: null
+      }
+      fileSelectedHandler = e => {
+        console.log("yes")
+        this.setState({
+          selectedFile: e.target.files[0]
+        })
+      }
+    
+      //fileUPloadHandler = () => {
+      //  image.post(' ')
+      //}
+    
+      render() {
+        return (
+          <div className="profileUserPicDiv">
+            <div className="profile-user-pic">
+              <label for="profileUserPic"></label>
+              <input type="file" onChange={this.fileChangeHandler} />
+              <button onClick={this.fileUploadHandler}>Upload a Photo</button>
+              
             </div>
-
-            
-            <div className="profile-user-bio-div">
-              <div className="profile-bio">
-                <label>
-                  About me:
-                 <textarea rows="4" cols="40" name="about" />
-                </label>
-              </div>
-            </div>
-
-
-          <div className="profile-Submit-Button">
-            <button>Submit</button>
           </div>
-
-        </form>
-        <DeleteAccount />
-        <NavLink exact to ="/" activeClassName="selected"><button>Feed</button></NavLink>
-      </div>
-    );
-  }
-}
-
-{/* class UserPic extends Component {
-  state = {
-    active: false,
-    selectedFile: null
-  }
-  fileSelectedHandler = e => {
-    console.log("yes")
-    this.setState({
-      selectedFile: e.target.files[0]
-    })
-  } */}
-
-  //fileUPloadHandler = () => {
-  //  image.post(' ')
-  //}
-
-//   render() {
-//     return (
-//       <div className="profileUserPicDiv">
-//         <div className="profile-user-pic">
-//           <label for="profileUserPic"></label>
-//           <input type="file" onChange={this.fileChangeHandler} />
-//           <button onClick={this.fileUploadHandler}>Upload a Photo</button>
-          
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// const mapStateToProps = state => {
-//   return {
-//     active: state.active
-//   }
-// }
-export default connect(
-  ({ /*auth,*/ users }) => ({
-    //loginInfo: auth.login,
-    username: users.currentUsername,
-    displayName: users.currentDisplayName,
-    about: users.currentAbout
-  }),
-  { user }
-)(ProfilePage);
+        )
+      }
+    }
+    
+    
+    const mapStateToProps = state => {
+      return {
+        active: state.active
+      }
+    }
+    export default connect(mapStateToProps, null)(ProfilePage)
