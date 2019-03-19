ac4f8f2e062950e83fe77e98a9709f2aac943e2d
KashF <Kashfarhadi@gmail.com>
2019-03-19 13:56:19 -0400

    added spacing to title

--- src/components/login/loginPage.js
+++ src/components/login/loginPage.js
@@ -3,14 +3,7 @@ import { connect } from "react-redux";
 import { loginThenGoToUserProfile as login } from "../../actions/auth";
 import Spinner from "react-spinkit";
 import { Link } from "react-router-dom";
-import {
-  Button,
-  Form,
-  Grid,
-  Image,
-  Input,
-  Segment
-} from "semantic-ui-react";
+import { Button, Form, Grid, Image, Input, Segment } from "semantic-ui-react";
 import logo from "../../logo.svg";
 
 class LoginPage extends Component {
@@ -33,59 +26,60 @@ class LoginPage extends Component {
         style={{ height: "100%" }}
         verticalAlign="middle"
       >
-      <Grid.Column style={{ maxWidth: 400 }}>
-        <Image src={logo} size="large" centered />
-        <div className="login-page">
-          <Form onSubmit={this.handleLogin} size="large">
-            <Segment stacked color="grey">
-              <h2>LOGIN:</h2>
-              <div className="login-user-name">
-                <Form.Field
-                  name="username"
-                  label="Username:"
-                  required
-                  placeholder="Username"
-                  type="text"
-                  control={Input}
-                  autoFocus
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="login-password">
-                <Form.Field
-                  name="password"
-                  label="Password:"
-                  required
-                  placeholder="Password"
-                  type="password"
-                  control={Input}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <Button.Group>
-                <Button
-                  className="login-button"
-                  type="submit"
-                  disabled={isLoading}
-                  positive
-                  size="large"
-                  to="/profile"
-                >
-                  Login
-                </Button>
-                <Button.Or />
+        <Grid.Column style={{ maxWidth: 400 }}>
+          <Image src={logo} size="large" centered />
+          <div className="login-page">
+            <Form onSubmit={this.handleLogin} size="large">
+              <Segment stacked color="grey">
+                <h2>LOGIN:</h2>
+                <div className="login-user-name">
+                  <Form.Field
+                    name="username"
+                    label="Username:"
+                    required
+                    placeholder="Username"
+                    type="text"
+                    control={Input}
+                    autoFocus
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="login-password">
+                  <Form.Field
+                    name="password"
+                    label="Password:"
+                    required
+                    placeholder="Password"
+                    type="password"
+                    control={Input}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <Button.Group>
+                  <br/>
+                  <Button
+                    className="login-button"
+                    type="submit"
+                    disabled={isLoading}
+                    positive
+                    size="large"
+                    to="/profile"
+                  >
+                    Login
+                  </Button>
+                  <Button.Or />
                   {/* <NavLink exact to ="/register" activeClassName="selected"><button>Register Now!</button></NavLink> */}
                   <Link exact to="/register">
-                    <Button className="signup-button" size="large" color="blue" >
+                    <Button className="signup-button" size="large" color="blue">
                       Register Now!
                     </Button>
                   </Link>
-                  </Button.Group>
-            </Segment>
-          </Form>
-          {isLoading && <Spinner name="circle" color="blue" />}
-          {err && <p style={{ color: "red" }}>{err}</p>}
-        </div>
+                </Button.Group>
+              </Segment>
+            </Form>
+            {isLoading && <Spinner name="circle" color="blue" />}
+            {err && <p style={{ color: "red" }}>{err}</p>}
+          </div>
         </Grid.Column>
       </Grid>
     );
